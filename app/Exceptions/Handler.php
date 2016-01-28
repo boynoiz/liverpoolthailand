c566846e8970f1ef1b51f4b61cc51c012e6ccc2c
Pathompong Pechkongtong <boynoiz@gmail.com>
2016-01-28 13:54:28 +0700

    Minor updates

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,7 +6,7 @@ use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Illuminate\Foundation\Validation\ValidationException;
+use Illuminate\Validation\ValidationException;
 use GrahamCampbell\Exceptions\ExceptionHandler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
