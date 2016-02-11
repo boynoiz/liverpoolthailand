4a401309c7ce95280baf456b5f487c68a52bfa1b
Pathompong Pechkongtong <boynoiz@gmail.com>
2016-02-11 19:58:11 +0700

    Move ImageService to AdminController and create ImagResizerJob, Pass process to resize after upload with Queue and use Beanstalkd Driver

---
+++ app/Jobs/ImageResizerJob.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace LTF\Jobs;
+
+use LTF\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Bus\SelfHandling;
+use Image;
+use File;
+
+class ImageResizerJob extends Job implements SelfHandling ,ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+    public function fire($job, $data)
+    {
+        $path = public_path() . $data['path'];
+        $fileName = $data['filename'];
+        $config = config('assets');
+        $resizeRatio = $config['ratio'];
+        $fullImagePath = $path . $fileName;
+        $image = Image::make($fullImagePath);
+        $sizes = $config['sizes'];
+        $image->backup();
+        foreach ($sizes as $size)
+        {
+            if ($size['name'] === 'thumb')
+            {
+                $image->fit($size['width'], $size['height']);
+            }
+            else
+            {
+                if(intval($image->width()/$resizeRatio) > $image->height())
+                {
+                    $image->fit(intval(($image->width() * $resizeRatio)), $image->height(), null, 'top');
+                    $image->resize($size['width'], $size['height']);
+                }
+                else
+                {
+                    $image->fit($image->width(), intval($image->width()/$resizeRatio), null, 'top');
+                    $image->resize($size['width'], $size['height']);
+                }
+            }
+            File::exists($path . strtolower($size['name'])) ? false : File::makeDirectory($path . strtolower($size['name']));
+            $image->save($path . strtolower($size['name']) .'/'. strtolower($size['name']) . '-'. $fileName, 70);
+            $image->reset();
+        }
+        $job->attempts() > 3 ? $job->delete() : $job->release(0);
+    }
+}
