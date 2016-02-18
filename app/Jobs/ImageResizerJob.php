12b1df85b57057ed26ca61b362c2c342be4dedf6
Pathompong Pechkongtong <boynoiz@gmail.com>
2016-02-18 21:29:52 +0700

    Move config into same file

--- app/Jobs/ImageResizerJob.php
+++ app/Jobs/ImageResizerJob.php
@@ -14,11 +14,42 @@ class ImageResizerJob extends Job implements SelfHandling ,ShouldQueue
 {
     use InteractsWithQueue, SerializesModels;
 
+    protected $config;
+
+    public function __construct()
+    {
+        $this->config = [
+            'sizes' => [
+                'thumb' => [
+                    'name' => 'thumb',
+                    'width' => '120',
+                    'height' => '120'
+                ],
+                'small' => [
+                    'name' => 'small',
+                    'width' => '320',
+                    'height' => '180'
+                ],
+                'medium' => [
+                    'name' => 'medium',
+                    'width' => '640',
+                    'height' => '360'
+                ],
+                'large' => [
+                    'name' => 'large',
+                    'width' => '1280',
+                    'height' => '720'
+                ]
+            ],
+            'ratio' => 16 / 9,
+        ];
+    }
+
     public function fire($job, $data)
     {
         $path = public_path() . $data['path'];
         $fileName = $data['filename'];
-        $config = config('assets');
+        $config = $this->config;
         $resizeRatio = $config['ratio'];
         $fullImagePath = $path . $fileName;
         $image = Image::make($fullImagePath);
