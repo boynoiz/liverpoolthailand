dc9d14d66d019348f6a1a5b617c9b76224f01ec9
Pathompong Pechkongtong <boynoiz@gmail.com>
2016-04-28 22:05:18 +0700

    Separate 'flag' to 'image_path' and 'image_name'

--- database/migrations/2015_04_06_131933_create_languages_table.php
+++ database/migrations/2015_04_06_131933_create_languages_table.php
@@ -14,11 +14,14 @@ class CreateLanguagesTable extends Migration
     {
         Schema::create('languages', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_id')->nullable();
             $table->string('title');
-            $table->string('flag');
             $table->string('code')->unique();
             $table->string('site_title');
             $table->string('site_description');
+            $table->string('image_path')->nullable();
+            $table->string('image_name')->nullable();
+            $table->unsignedInteger('update_by')->nullable();
             $table->timestamps();
         });
     }
