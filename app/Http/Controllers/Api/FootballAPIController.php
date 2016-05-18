f7770a429007e65635e85d30422edf22c2908cb2
Pathompong Pechkongtong <boynoiz@gmail.com>
2016-05-18 19:23:49 +0700

    Create FootballAPIController for sending api data to homepage

---
+++ app/Http/Controllers/Api/FootballAPIController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LTF\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+
+use LTF\FootballMatches;
+use Jenssegers\Date\Date;
+use LTF\Http\Requests;
+use LTF\Http\Controllers\Controller;
+
+class FootballAPIController extends Controller
+{
+
+    /**
+     * @param FootballMatches $matches
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function live(FootballMatches $matches)
+    {
+        $match = $matches
+            ->with(['events', 'competition', 'team_as_home', 'team_as_away'])
+            ->whereNotIn('status', ['FT', 'Postp.'])
+            ->whereDate('formatted_date', '>=', Date::yesterday()->format('Y-m-d'))
+            ->orderBy('formatted_date', 'asc')
+            ->first();
+
+        return response()->json($match->toArray());
+    }
+}
