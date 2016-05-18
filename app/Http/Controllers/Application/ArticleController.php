48f674552f2c0bc4074bbd62e84490d596137db5
Pathompong Pechkongtong <boynoiz@gmail.com>
2016-05-18 19:32:27 +0700

    Add fbLikeCounter and totalMembers that needed in the footer

--- app/Http/Controllers/Application/ArticleController.php
+++ app/Http/Controllers/Application/ArticleController.php
@@ -5,10 +5,33 @@ namespace LTF\Http\Controllers\Application;
 use LTF\Article;
 use LTF\Events\ArticleWasViewed;
 use LTF\Base\Controllers\ApplicationController;
+use LTF\Http\Controllers\Application\FacebookController;
+use LTF\Http\Controllers\Application\IPBoardController;
 use Event;
 
 class ArticleController extends ApplicationController
 {
+    /**
+     * @var \LTF\Http\Controllers\Application\FacebookController
+     */
+    public $fbLikes;
+
+    /**
+     * @var \LTF\Http\Controllers\Application\IPBoardController
+     */
+    public $ipb;
+
+    /**
+     * ArticleController constructor.
+     * @param \LTF\Http\Controllers\Application\FacebookController $fbLikes
+     * @param \LTF\Http\Controllers\Application\IPBoardController $ipb
+     */
+    public function __construct(FacebookController $fbLikes, IPBoardController $ipb)
+    {
+        $this->fbLikes = $fbLikes;
+        $this->ipb = $ipb;
+    }
+
     /**
      * Show the article.
      *
@@ -18,6 +41,8 @@ class ArticleController extends ApplicationController
     public function index(Article $article)
     {
         Event::fire(new ArticleWasViewed($article));
-        return view('application.article.index', compact('article'));
+        $fbLikeCounter = $this->fbLikes->facebookLike();
+        $totalMembers = $this->ipb->getTotalMembers();
+        return view('application.article.index', compact('article', 'fbLikeCounter', 'totalMembers'));
     }
 }
