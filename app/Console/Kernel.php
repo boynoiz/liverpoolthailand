e0fd24f2e831eb5d5cfbd5a50642795b1de8ab3d
Pathompong Pechkongtong <boynoiz@gmail.com>
2016-05-18 19:03:50 +0700

    Add Task Scheduling for fetching data from api when the game is live

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace LTF\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use LTF\FootballMatches;
+use Jenssegers\Date\Date;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,9 +26,21 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call('LTF\Http\Controllers\Admin\FootballController@createOrUpdateFixtures')->hourly();
+        $schedule->call('LTF\Http\Controllers\Admin\FootballController@LiveMatch')->everyMinute()->when(function()
+        {
+            $match = FootballMatches::whereNotIn('status', ['FT', 'Postp.'])
+                ->whereDate('formatted_date', '>=', Date::yesterday()->format('Y-m-d'))
+                ->orderBy('formatted_date', 'asc')
+                ->first();
+            $gameMinute = $match->status === 'AET' ? 180 : 105;
+            $timeKickoff = Date::createFromFormat('Y-m-d H:i', $match->formatted_date . $match->time);
+            $gameFinishTime = Date::createFromFormat('Y-m-d H:i', $match->formatted_date . $match->time)->addMinutes($gameMinute);
+
+            return Date::now()->between($timeKickoff, $gameFinishTime) ? true : false;
+        });
         $schedule->call('LTF\Http\Controllers\Admin\FootballController@createOrUpdateStanding')->hourly();
+        $schedule->call('LTF\Http\Controllers\Admin\FootballController@createOrUpdateFixtures')->hourly();
+        $schedule->call('LTF\Http\Controllers\Admin\FootballController@updateOrCreateTeams')->daily();
         $schedule->call('LTF\Http\Controllers\Admin\FootballController@updateOrCreateComp')->weekly();
-        $schedule->call('LTF\Http\Controllers\Admin\FootballController@updateOrCreateTeams')->weekly();
     }
 }
