1f4caf0c930faa83d15f457c52d127afe0f2a4f5
Pathompong Pechkongtong <boynoiz@gmail.com>
2016-05-27 16:19:19 +0700

    Missing Image path

--- resources/views/partials/default/galleryImages/mainImagesGallery.blade.php
+++ resources/views/partials/default/galleryImages/mainImagesGallery.blade.php
@@ -5,7 +5,7 @@
 <div class="col_full masonry-thumbs col-6 bottommargin-lg clearfix" data-big="5" data-lightbox="gallery">
     @if(!empty($lastGalleryImages))
         @foreach($lastGalleryImages as $gallery)
-            <a href="{!! url('http://board.liverpoolthailand.com/uploads/' . $gallery->directory , $gallery->masked_file_name) !!}" data-lightbox="gallery-item">
+            <a href="{!! url('http://board.liverpoolthailand.com/uploads/' . $gallery->directory .'/'. $gallery->masked_file_name) !!}" data-lightbox="gallery-item">
                 <img class="image_fade" src="{!! url('http://board.liverpoolthailand.com/uploads/' . $gallery->directory .'/tn_' . $gallery->masked_file_name) !!}" alt="{!! $gallery->caption !!}" title="{!! $gallery->caption !!}">
             </a>
         @endforeach
