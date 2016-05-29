b29f4e81ae10885252c1ab1b386f2aea9e163694
Pathompong Pechkongtong <boynoiz@gmail.com>
2016-05-29 21:47:46 +0700

    Added Route to accept image request and pass through to the Glide Images

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,7 +6,7 @@ Route::group(['middleware' => 'web'], function () {
         Route::get('/', ['as' => 'root', 'uses' => 'MainController@index']);
         Route::get('no-transition', ['as' => 'root', 'uses' => 'MainController@index']);
         Route::get('home', ['as' => 'root', 'uses' => 'MainController@index']);
-        Route::get('board', function (){
+        Route::get('board', function () {
             return redirect(url('http://board.liverpoolthailand.com'));
         });
         Route::get('articles', ['as' => 'article', 'uses' => 'ArticleController@index']);
@@ -14,6 +14,15 @@ Route::group(['middleware' => 'web'], function () {
         Route::get('page/{page_slug}', ['as' => 'page', 'uses' => 'PageController@index']);
         Route::get('category/{category_slug}', ['as' => 'category', 'uses' => 'CategoryController@index']);
         Route::post('language/change', ['as' => 'app.language.change' , 'uses' => 'LanguageController@postChange']);
+        Route::get('assets/images/{source}/cache/{path}', function ($source, $path, Illuminate\Http\Request $request) {
+            $server = League\Glide\ServerFactory::create([
+                'source' => Storage::disk($source)->getDriver(),
+                'cache' => Storage::disk('local')->getDriver(),
+                'cache_path_prefix' => 'images/.cache',
+                'base_url' => 'assets/images/'. $source . '/cache',
+            ]);
+            $server->outputImage($path, $request->all());
+        })->where('path', '.+');
     });
     // Auth routes
     Route::group(['namespace' => 'Auth'], function () {
