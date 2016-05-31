8f813f1d3e7289e1b8481e6efff694bbf07a1d2d
Pathompong Pechkongtong <boynoiz@gmail.com>
2016-05-31 22:52:08 +0700

    Move entry section to new partials/article

--- resources/views/application/article/index.blade.php
+++ resources/views/application/article/index.blade.php
@@ -27,23 +27,7 @@
                         <div id="posts">
                             @if(!empty($articles))
                                 @foreach($articles as $article)
-                                    <div class="entry clearfix">
-                                        <div class="entry-image">
-                                            <a href="{{ route('article.show', ['article_slug' => $article->slug]) }}" data-lightbox="image" target="_blank"><img class="image_fade" src="{{ asset($article->image_path .'medium/'. $article->image_name) }}" alt="Standard Post with Image"></a>
-                                        </div>
-                                        <div class="entry-title">
-                                            <h2><a href="{{ route('article.show', ['article_slug' => $article->slug]) }}" target="_blank">{{ $article->title }}</a></h2>
-                                        </div>
-                                        <ul class="entry-meta clearfix">
-                                            <li><i class="icon-calendar3"></i> {{ Date::createFromFormat('Y-m-d', $article->published_at)->formatLocalized('%A %d %B %Y') }}</li>
-                                            <li><i class="icon-user"></i> {{ $article->user->name }}</li>
-                                            <li><i class="icon-folder-open"></i> <a href="{{ route('category', ['category_slug' => $article->category->slug]) }}"> {{ $article->category->title }}</a></li>
-                                        </ul>
-                                        <div class="entry-content">
-                                            {!! str_limit($article->content, 300)  !!}
-                                            <a href="{{ route('article', ['article_slug' => $article->slug]) }}" target="_blank">Read more</a>
-                                        </div>
-                                    </div>
+                                    @include('partials.default.article.post')
                                 @endforeach
                             @endif
                         </div>
--- resources/views/application/category/index.blade.php
+++ resources/views/application/category/index.blade.php
@@ -16,7 +16,6 @@
         </div>
     </section>
     <!-- #page-title end -->
-
     <!-- Content -->
     <section id="content">
         <div class="content-wrap">
@@ -27,23 +26,7 @@
                     <div id="posts">
                         @if(!empty($articles))
                             @foreach($articles as $article)
-                                <div class="entry clearfix">
-                                    <div class="entry-image">
-                                        <a href="{{ route('article.show', ['article_slug' => $article->slug]) }}" data-lightbox="image" target="_blank"><img class="image_fade" src="{{ asset($article->image_path .'medium/'. $article->image_name) }}" alt="Standard Post with Image"></a>
-                                    </div>
-                                    <div class="entry-title">
-                                        <h2><a href="{{ route('article.show', ['article_slug' => $article->slug]) }}" target="_blank">{{ $article->title }}</a></h2>
-                                    </div>
-                                    <ul class="entry-meta clearfix">
-                                        <li><i class="icon-calendar3"></i> {{ Date::createFromFormat('Y-m-d', $article->published_at)->formatLocalized('%A %d %B %Y') }}</li>
-                                        <li><i class="icon-user"></i> {{ $article->user->name }}</li>
-                                        <li><i class="icon-folder-open"></i> <a href="{{ route('category', ['category_slug' => $article->category->slug]) }}"> {{ $article->category->title }}</a></li>
-                                    </ul>
-                                    <div class="entry-content">
-                                        {!! str_limit($article->content, 300)  !!}
-                                        <a href="{{ route('article', ['article_slug' => $article->slug]) }}" target="_blank">Read more</a>
-                                    </div>
-                                </div>
+                                @include('partials.default.article.post')
                             @endforeach
                         @endif
                     </div>
