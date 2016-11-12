9ba816a0a00a2d9fc62772488dc66a4b58b79f7f
Pathompong Pechkongtong <boynoiz@gmail.com>
2016-11-12 19:28:24 +0700

    Fix email notification that not compatible with Illuminate\Auth\Notifications\ResetPassword::toMail($notifiable)

--- app/Base/Auth/ResetPassword.php
+++ app/Base/Auth/ResetPassword.php
@@ -8,18 +8,29 @@ use Illuminate\Notifications\Messages\MailMessage;
 class ResetPassword extends BaseResetPassword
 {
     /**
+     * Get the notification's channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array|string
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
      * Build the mail representation of the notification.
      *
+     * @param mixed $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
-    public function toMail()
+    public function toMail($notifiable)
     {
-        $message = (new MailMessage)
+        return (new MailMessage)
             ->subject(trans('auth.password.email.action'))
             ->greeting(trans('auth.password.email.introduction'))
             ->line(trans('auth.password.email.content'))
             ->action(trans('auth.password.email.action'), route('password.reset.token', ['token' => $this->token]))
             ->line(trans('auth.password.email.conclusion'));
-        return $message;
     }
 }
